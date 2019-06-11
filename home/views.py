165dafc9a9b3d8c198b824ab49159674e2f6f78c
doriask <doriss_91@hotmail.com>
2019-06-11 13:51:14 +0200

    mise en place de la logique views et template de la home

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.template import loader
 from rental.models import Rental
+from .models import Administrator
 
 
 # TO DO: FILTRER LA VUE POUR LES ADMINS ONLY + FILTRER REQUETE SUR L'ADMIN EN LIGNE
@@ -9,17 +10,23 @@ def home(request):
     """ Get the homepage and display the information for the dashboard """
     if request.method == 'GET':
         dash = []
+        admin_dashboard = Administrator.objects.filter(user__last_name='test')
+        print(admin_dashboard, 'admin')
 
-        for item in Rental.objects.filter(archived=False):
-            details = {
-                'occupant': item.occupant,
-                'rent_amount': item.rent_amount,
-                'charges': item.charges,
-                'property': item.property,
-            }
-            dash.append(details)
-            print(details)
-            context = {'dash': dash}
+        # dashboard = Rental.objects.filter(archived=False, property__administrator__in=admin_dashboard)
+        # print(dashboard, 'dash')
+        #
+        # for item in dashboard:
+        #     print(item, 'item')
+        #     details = {
+        #         'occupant': item.occupant,
+        #         'rent_amount': item.rent_amount,
+        #         'charges': item.charges,
+        #         'property': item.property,
+        #     }
+        #     dash.append(details)
+        #     print(details)
+        context = {'admin_dash': admin_dashboard}
         # print(item.property.administrator.user.first_name)
 
         template = loader.get_template('home/index.html')
