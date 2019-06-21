5b2ca8786592a164976c473454535801247e089f
doriask <doriss_91@hotmail.com>
2019-06-21 18:35:02 +0200

    mise en place prod.py

---
+++ immocool/settings/production.py
@@ -0,0 +1,23 @@
+from . import *
+import os
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+DEBUG = False
+ALLOWED_HOSTS = ['104.248.136.26']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PWD'],
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+ADMINS = os.environ['ADMNS']
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_PORT = os.environ['EMAIL_PORT']
