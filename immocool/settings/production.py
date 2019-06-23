51a9e474102e3e00774c6d8aac5b4846d139b7cb
doriask <doriss_91@hotmail.com>
2019-06-23 14:34:41 +0200

    modif allowed host

--- immocool/settings/production.py
+++ immocool/settings/production.py
@@ -4,7 +4,7 @@ from os import environ
 
 SECRET_KEY = os.getenv("IMMOCOOL_SECRET_KEY")
 DEBUG = False
-ALLOWED_HOSTS = ['immocool.matxik.com', '104.248.136.26']
+ALLOWED_HOSTS = ['immocool.matxik.com', '104.248.136.26:8001']
 
 DATABASES = {
     'default': {
