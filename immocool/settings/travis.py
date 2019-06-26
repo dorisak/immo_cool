f42e0eb76dafc3b1ffe80f5212328695b78a0a6d
doriask <doriss_91@hotmail.com>
2019-06-26 22:25:47 +0200

    travis config

--- immocool/settings/travis.py
+++ immocool/settings/travis.py
@@ -7,7 +7,7 @@ SECRET_KEY = config('SECRET_KEY')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'immocool',
+        'NAME': 'travis_ci_test',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '',
