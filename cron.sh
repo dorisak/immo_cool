f8bd09ae9a9a24d458913ca92994397223449373
doriask <doriss_91@hotmail.com>
2019-06-26 23:35:03 +0200

    modif pr cron

---
+++ cron.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PY=/home/doris/.local/share/virtualenvs/immo_cool-UOny7QwX/bin/python
+export SECRET_KEY="ck8k8bzjqxgsqxax_!na5!s0nd$a2?"
+export DJANGO_SETTINGS_MODULE=immocool.settings.production
+export IMMOCOOL_DB_NAME="immocool"
+export IMMOCOOL_DB_USER="adminic"
+export IMMOCOOL_DB_PWD="Pikool84"
+
+cd /home/doris/immo_cool
+$PY manage.py alert_admin --settings=$DJANGO_SETTINGS_MODULE
