18132da90856be0d6e2ba35d6a4f272794385a5d
doriask <doriss_91@hotmail.com>
2019-06-27 18:35:28 +0200

    modif pr travis

--- immocool/settings/travis.py
+++ immocool/settings/travis.py
@@ -1,16 +1,18 @@
+import os
 from .base import *
 from decouple import config
 
+if 'TRAVIS' in os.environ:
 
-SECRET_KEY = config('SECRET_KEY')
+    SECRET_KEY = config('SECRET_KEY')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'travis_ci_test',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    },
-}
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'travis_ci_test',
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        },
+    }
