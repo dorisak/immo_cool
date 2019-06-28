b53549b891283187d6a8dfdad372bd19fe193bd4
doriask <doriss_91@hotmail.com>
2019-06-28 10:20:59 +0200

    modif pour cron echeance

--- cron.sh
+++ cron.sh
@@ -6,6 +6,11 @@ export DJANGO_SETTINGS_MODULE=immocool.settings.production
 export IMMOCOOL_DB_NAME="immocool"
 export IMMOCOOL_DB_USER="adminic"
 export IMMOCOOL_DB_PWD="Pikool84"
+export IMMOCOOL_ADMNS=
+export IMMOCOOL_EMAIL_HOST=
+export IMMOCOOL_EMAIL_HOST_USER=
+export IMMOCOOL_EMAIL_HOST_PASSWORD=
+export IMMOCOOL_EMAIL_PORT=
 
 cd /home/doris/immo_cool
 $PY manage.py echeance_creation --settings=$DJANGO_SETTINGS_MODULE
